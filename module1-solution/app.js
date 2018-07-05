c22f2c5cee3325b0219418b5323c4fc98506814c
lrela <rel@iki.fi>
2018-07-05 19:25:12 +0300

    fixed

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,15 +8,16 @@ LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController ($scope) {
 	$scope.checkLunch = function () {
-		if($scope.lunch === undefined || $scope.lunch.length === 0)
+		if($scope.lunch === undefined || $scope.lunch.length === 0) {
 			$scope.result = "Please enter data first"
-		else {
-			var items = $scope.lunch.split(",");
-			if(items.length > 3)
-				$scope.result = "Too much!"
-			else
-				$scope.result = "Enjoy!"
+			return;
 		}
+
+		var items = $scope.lunch.split(",");
+		if(items.length > 3)
+			$scope.result = "Too much!"
+		else
+			$scope.result = "Enjoy!"
 	};
 }
 

f7ba403987e887d21cd899ca2843bd5ebc582efa
lrela <rel@iki.fi>
2018-07-06 16:26:43 +0300

    do not count empty string

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,16 +8,24 @@ LunchCheckController.$inject = ['$scope'];
 
 function LunchCheckController ($scope) {
 	$scope.checkLunch = function () {
-		if($scope.lunch === undefined || $scope.lunch.length === 0) {
+		if($scope.lunch === undefined || $scope.lunch.length == 0) {
 			$scope.result = "Please enter data first"
 			return;
 		}
 
 		var items = $scope.lunch.split(",");
-		if(items.length > 3)
-			$scope.result = "Too much!"
-		else
-			$scope.result = "Enjoy!"
+		
+		var itemCount = 0;
+		for(var i = 0; i < items.length; i++)
+			if(items[i].trim().length > 0) {
+				if(itemCount == 3) {
+					$scope.result = "Too much!";
+					return;
+				}				
+				itemCount++;
+			}
+		
+		$scope.result = "Enjoy!"
 	};
 }
 

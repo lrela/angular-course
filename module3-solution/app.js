11731f1823545c64f5ce4e92e2eff9b4872d6950
rela <leo.rela@gmail.com>
2018-08-18 22:29:31 +0300

    a

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,52 +3,50 @@
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService);
+.service('MenuSearchService', MenuSearchService)
+.constant('Url', "https://davids-restaurant.herokuapp.com/menu_items.json");
 
-NarrowItDownController.$inject = ['MenuSearchService', '$http'];
+NarrowItDownController.$inject = ['MenuSearchService'];
 
 function NarrowItDownController(MenuSearchService) {
 	
 	// tänne promise MenuSearchServicelle
-	
 	var buyCtrl = this;
 	buyCtrl.buy = function buy(index) {
 		MenuSearchService.buy(index);
 	}
 
-	buyCtrl.items = MenuSearchService.getMatchedMenuItems();
-	
+	buyCtrl.items = MenuSearchService.getMatchedMenuItems('sauce');	
 }
 
-
-function MenuSearchService() {
+MenuSearchService.$inject = ['$http', 'Url'];
+function MenuSearchService($http, url) {
 	var service = this;
-	
+
 	service.getMatchedMenuItems = function getMatchedMenuItems(searchTerm) {
 		return $http({
-			url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+			url: url
 		}).then(function (response) {
 			// process result and only keep items that match
-			var foundItems = response.data;
-			console.log(foundItems);
 
-			// return processed items
-			return foundItems;
-		});
+			var items = response.data.menu_items;
+			var found = [];
+
+			for(var i = 0; i < items.length; i++) {
+				//console.log(items[i].description);
+				if(items[i].description.includes(searchTerm)) {
+					found.push(items[i]);
+					console.log(items[i]);
+				}
+			}
+
+			console.log("found " + found.length);
+			return found;
+
+		}).catch(function (error) {
+			console.log(error);
+		})
 	};
-	
 }
-
-
-/*
-function MenuSearchService() {
-	var service = this;
-	
-	service.getMatchedMenuItems = function getMatchedMenuItems(searchTerm) {
-		return toBuyItems;
-	};
-	
-}
-*/
 
 })();

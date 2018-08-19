380573d9a2725932331b53f2d4071003b3a1bd5b
rela <leo.rela@gmail.com>
2018-08-19 23:57:05 +0300

    a

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,8 +4,10 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('Url', "https://davids-restaurant.herokuapp.com/menu_items.json")
+.constant('Url', "http://localhost/module3-solution/menu.json")
 .directive('foundItems', FoundItemsDirective);
+
+//https://davids-restaurant.herokuapp.com/menu_items.json
 
 function FoundItemsDirective() {
   var ddo = {
@@ -13,11 +15,14 @@ function FoundItemsDirective() {
 	restrict: 'E',
     scope: {
       items: '<',
+      remove: '&'
+	  
 	  /*
       myTitle: '@title',
       badRemove: '=',
       onRemove: '&'
 	  */
+	  
     },
 	/*
     controller: NarrowItDownController,
@@ -42,9 +47,14 @@ function NarrowItDownController($scope, MenuSearchService) {
 		promise.then(function(result) {
 			ctrl.found = result;
 			console.log('items len ' + ctrl.found.length);
-		})
+		});
 		
-	}
+	};
+	
+	ctrl.remove = function(index) {
+		console.log("remove" + index);
+		ctrl.found.splice(index, 1);
+    };
 }
 
 MenuSearchService.$inject = ['$http', 'Url'];
@@ -59,12 +69,11 @@ function MenuSearchService($http, url) {
 			var items = response.data.menu_items;
 			var found = []
 
-			for(var i = 0; i < items.length;) {
-				if(items[i].description.includes(searchTerm)) {
-					//console.log(items[i].name);
-					found.push(items[i++]);
-					//console.log(found.pop());
-
+			var len = items.length;
+			for(var i = 0; i < len; i++) {
+				if(items[i].description.indexOf(searchTerm) != -1) {
+					//console.log(items[i].description);
+					found.push(items[i]);
 				}
 			}
 	

809730fb0281449f04c2229337aaf886f4bb4f34
lrela <rel@iki.fi>
2018-08-19 18:58:57 +0300

    a

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -4,7 +4,43 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('Url', "https://davids-restaurant.herokuapp.com/menu_items.json");
+.constant('Url', "https://davids-restaurant.herokuapp.com/menu_items.json")
+.directive('foundItems', FoundItemsDirective);
+
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+      myTitle: '@title',
+      badRemove: '=',
+      onRemove: '&'
+    },
+    controller: ShoppingListDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
+  };
+
+  return ddo;
+}
+
+
+function FoundItemsDirectiveController() {
+  var list = this;
+
+  list.cookiesInList = function () {
+    for (var i = 0; i < list.items.length; i++) {
+      var name = list.items[i].name;
+      if (name.toLowerCase().indexOf("cookie") !== -1) {
+        return true;
+      }
+    }FoundItemsDirective
+
+    return false;
+  };
+}
+
 
 NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 function NarrowItDownController($scope, MenuSearchService) {
